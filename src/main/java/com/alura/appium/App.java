f12b181286fd55f9a1eb8383db299fce67e7d334
Elza Simoes De Oliveira/SEL (Brazil)/Associate/Samsung Electronics <elza.bisneta@samsung.com>
2021-01-31 18:25:38 -0400

    Initial commit

---
+++ src/main/java/com/alura/appium/App.java
@@ -0,0 +1,28 @@
+package com.alura.appium;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.File;
+
+
+public class App
+{
+    public static void main( String[] args ) throws MalformedURLException {
+
+        File apk = new File ( "D:\\Appium\\Alura-appium\\src\\main\\resources\\alura_esporte.apk");
+
+        DesiredCapabilities configuracoes = new DesiredCapabilities();
+        configuracoes.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
+        configuracoes.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        configuracoes.setCapability(MobileCapabilityType.AUTOMATION_NAME,  "UiAutomator2");
+
+        URL urlConexao = new URL(  "http://127.0.0.1:4723/wd/hub");
+        AppiumDriver driver = new AppiumDriver<>(urlConexao, configuracoes);
+
+}
+}
